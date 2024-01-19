78549bbd523c3ac5803088f2c6236d3e74708541
Rob Nugen <rob@robnugen.com>
2024-01-19 17:58:18 +0900

    Gotta have my print_rob

--- prepend.php
+++ prepend.php
@@ -5,6 +5,20 @@ require_once __DIR__ . '/classes/Mlaphp/Autoloader.php';
 $autoloader = new \Mlaphp\Autoloader();
 spl_autoload_register(array($autoloader, 'load'));
 
+function print_rob($object, $exit = true)
+{
+    echo ("<pre>");
+    if (is_object($object) && method_exists($object, "toArray")) {
+        echo "ResultSet => " . print_r($object->toArray(), true);
+    } else {
+        print_r($object);
+    }
+    echo ("</pre>");
+    if ($exit) {
+        exit;
+    }
+}
+
 $config = new \Config();
 echo "Prepend.php loaded.";
 echo "<br>" . $config->dbHost;
