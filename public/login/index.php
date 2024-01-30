2ce37f828360a3361bdb51494654dc42c43c46c3
Rob Nugen <rob@robnugen.com>
2024-01-30 18:33:17 +0900

    Document boilerplate

--- public/login/index.php
+++ public/login/index.php
@@ -1,7 +1,7 @@
 <?php
 
-# Prefer to do this via .htaccess, but this works for now
-include_once("../../prepend.php");
+# Must include here because DH runs FastCGI https://www.phind.com/search?cache=zfj8o8igbqvaj8cm91wp1b7k
+include_once("/home/quill_dh_plasz3gi/quill.plasticaddy.com/prepend.php");
 
 $page = new \Template($mla_request, $mla_database);
 $page->setTemplate("login/index.tpl.php");
