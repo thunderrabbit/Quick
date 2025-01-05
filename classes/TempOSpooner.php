749c651a8e7f0ab34f2c5de27a19bf50b4e47830
Rob Nugen <rob@robnugen.com>
2025-01-05 21:52:09 +0900

    BEGIN big update to ensure correct branches are used

--- classes/TempOSpooner.php
+++ classes/TempOSpooner.php
@@ -6,7 +6,56 @@ class TempOSpooner
     {
     }
 
+    private function getDateOfCurrentHEAD(): string
+    {
+        // Get the date of the current HEAD
+        echo "<p>Getting the date of the current HEAD\n</p>";
+        exec(command: "git show -s --format=%ci HEAD", output: $dateOutput);
+        $date = trim(string: implode(separator: "\n", array: $dateOutput));
+        echo "<p>Date of current HEAD: $date\n</p>";
+        return $date;
+    }
 
+    private function getOntoCorrectBranch(): void
+    {
+        // Check the current branch
+        echo "<p>Checking current branch\n</p>";
+        exec("git rev-parse --abbrev-ref HEAD", $currentBranchOutput);
+        $currentBranch = trim(implode("\n", $currentBranchOutput));
+
+        $onMasterBranch = false;
+        // Check if the current branch starts with 'tempo'
+        echo "<p>Checking if current branch $currentBranch starts with 'tempo'\n</p>";
+        if (strpos($currentBranch, needle: 'tempo') === 0) {
+            $tempoBranchName = $currentBranch;
+            $dateTempBranch = $this->getDateOfCurrentHEAD();
+        } elseif (strpos(haystack: $currentBranch, needle: 'master') === 0) {
+            echo "<p>We are on the master branch already.";
+            $onMasterBranch = true;
+        } else {
+            throw new Exception(message: "You are not on a branch starting with 'tempo' nor 'master'.");
+        }
+
+        if (! $onMasterBranch) {
+            // Switch to the master branch
+            echo "<p>Switching to master branch\n</p>";
+            exec("git checkout master", $output);
+            if (strpos(implode("\n", $output), needle: "Switched to branch 'master'") === false) {
+                throw new Exception(message: "Failed to switch to master branch: " . implode("\n", $output));
+            } else {
+                echo "<p>Successfully switched to master branch\n</p>";
+                $onMasterBranch = true;
+            }
+        }
+
+        echo "<p>Checking out master branch and pulling it\n</p>";
+        exec("git pull", $output);
+        $dateMasterBranch = $this->getDateOfCurrentHEAD();
+
+        echo "<p>Date of master branch: $dateMasterBranch\n</p>";
+
+
+    }
     public function addAndPushToGit($filePath, $config): string
     {
         // Use the repository path from the config
@@ -19,32 +68,29 @@ class TempOSpooner
         $output = [];
         $returnVar = 0;
 
-        // Add the file to the git index
-        echo "<p>Adding $filePath to git repository at $repositoryPath\n</p>";
-        $returnVar = exec(command: "git add " . escapeshellarg(arg: $filePath), output: $output);
-        if (!empty($returnVar)) {
-            $errorOutput = implode(separator: "\n", array: $output);  // Merge all lines of output into a single string
-            throw new Exception(message: "Failed to add file to git: " . ($errorOutput ?: "No output returned") . ($returnVar ? " (Return code: $returnVar)" : ""));
-        }
+        // git checkout master branch and pull it
+        // echo "<p>Checking out master branch and pulling it\n</p>";
+        // $returnVar = exec("git checkout master", $output);
+        // $returnVar = exec("git pull", $output);
 
-        // Commit the changes
-        echo "<p>Committing changes\n</p>";
-        $returnVar = exec(command: "git commit -m 'Add new journal entry'", output: $output);
-        if (!str_starts_with(haystack: $returnVar, needle: " create mode 100644")) {
-            $errorOutput = implode(separator: "\n", array: $output);  // Merge all lines of output into a single string
-            throw new Exception(message: "Failed >$returnVar< to commit changes: " . ($errorOutput ?: "No output returned") . ($returnVar ? " (Return code: $returnVar)" : ""));
-        }
 
-        // Check the current branch
-        echo "<p>Checking current branch\n</p>";
-        $returnVar = exec("git rev-parse --abbrev-ref HEAD", $currentBranchOutput);
-        $currentBranch = trim(implode("\n", $currentBranchOutput));
+        // // Add the file to the git index
+        // echo "<p>Adding $filePath to git repository at $repositoryPath\n</p>";
+        // $returnVar = exec(command: "git add " . escapeshellarg(arg: $filePath), output: $output);
+        // if (!empty($returnVar)) {
+        //     $errorOutput = implode(separator: "\n", array: $output);  // Merge all lines of output into a single string
+        //     throw new Exception(message: "Failed to add file to git: " . ($errorOutput ?: "No output returned") . ($returnVar ? " (Return code: $returnVar)" : ""));
+        // }
 
-        // Check if the current branch starts with 'tempo'
-        echo "<p>Checking if current branch starts with 'tempo'\n</p>";
-        if (strpos($currentBranch, 'tempo') === 0) {
-            $oldBranchName = $currentBranch;
-        }
+        // // Commit the changes
+        // echo "<p>Committing changes\n</p>";
+        // $returnVar = exec(command: "git commit -m 'Add new journal entry'", output: $output);
+        // if (!str_starts_with(haystack: $returnVar, needle: " create mode 100644")) {
+        //     $errorOutput = implode(separator: "\n", array: $output);  // Merge all lines of output into a single string
+        //     throw new Exception(message: "Failed >$returnVar< to commit changes: " . ($errorOutput ?: "No output returned") . ($returnVar ? " (Return code: $returnVar)" : ""));
+        // }
+
+        $this->getOntoCorrectBranch();
 
         // Create a new random branch name starting with 'tempo'
         $newBranchName = 'tempo_' . uniqid();
@@ -75,9 +121,14 @@ class TempOSpooner
 
         // Push the changes to the new branch
         echo "<p>Pushing changes to remote for new branch $newBranchName\n</p>";
-        $returnVar = exec(command: "git push origin $newBranchName");
+        echo "<pre>git push origin $newBranchName</pre>";
+        $returnVar = exec(command: "git push origin $newBranchName", output: $outputOfPush);
+        echo "<pre>outputOfPush: " . print_r($outputOfPush, true) . "</pre>";
         if (!empty($returnVar)) {
+            echo "Failed >$returnVar< to push changes to remote: " . implode("\n", $output);
             throw new Exception("Failed to push >$returnVar< changes to remote: " . implode("\n", $output));
+        } else {
+            echo "<p>Changes >$returnVar< successfully pushed to remote for new branch $newBranchName\n</p>";
         }
 
         return $newBranchName;   // to be used in the message displayed to the user
