dbe42d0af282075746ba14d5af2cf3e86ae3432b
Rob Nugen <rob@robnugen.com>
2025-01-09 17:19:21 +0900

    Remove superfluous parenthesis

--- public/login/index.php
+++ public/login/index.php
@@ -1,7 +1,7 @@
 <?php
 
 # Must include here because DH runs FastCGI https://www.phind.com/search?cache=zfj8o8igbqvaj8cm91wp1b7k
-include_once("/home/barefoot_rob/quick.robnugen.com/prepend.php");
+include_once "/home/barefoot_rob/quick.robnugen.com/prepend.php";
 
 if ($is_logged_in->isLoggedIn()) {
     $page = new \Template($mla_request, $mla_database, $config);
