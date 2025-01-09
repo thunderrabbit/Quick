6e4eaa6e12e23d60fa84d786a1ee3800af31278a
Rob Nugen <rob@robnugen.com>
2025-01-09 21:04:45 +0900

    Create a page to instantiate QuickDeployer

---
+++ public/deploy/index.php
@@ -0,0 +1,21 @@
+<?php
+
+# Must include here because DH runs FastCGI https://www.phind.com/search?cache=zfj8o8igbqvaj8cm91wp1b7k
+include_once "/home/barefoot_rob/quick.robnugen.com/prepend.php";
+
+if ($mla_request->post) {
+    $postifier = new \QuickDeployer(debug: $mla_request->post['debug_deploy']);
+    $okay = $postifier->pushToBranch(newBranchName: $mla_request->post['branch']);
+    if($okay)
+    {
+    }
+}
+
+$page = new \Template(config: $config);
+if(isset($post_path))
+{
+    $page->set(name: "post_path", value: $post_path);
+}
+$page->setTemplate(template_file: "poster/index.tpl.php");
+$page->set(name: "text", value: "");
+$page->echoToScreen();
