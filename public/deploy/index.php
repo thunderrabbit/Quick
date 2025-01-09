13f3b0f7cdabfbf07fed6cad38e58a6f7ba8c97d
Rob Nugen <rob@robnugen.com>
2025-01-09 21:35:11 +0900

    Confirm we are on correct branch

--- public/deploy/index.php
+++ public/deploy/index.php
@@ -3,9 +3,25 @@
 # Must include here because DH runs FastCGI https://www.phind.com/search?cache=zfj8o8igbqvaj8cm91wp1b7k
 include_once "/home/barefoot_rob/quick.robnugen.com/prepend.php";
 
+// Use the repository path from the config
+$repositoryPath = $config->post_path_journal;
+
+// Change directory to the repository path
+chdir(directory: $repositoryPath);
+
 if ($mla_request->post) {
+    $mrBranchFactory = new MrBranchFactory($mla_request->post["debug_deploy"]);
+    $currentMrBranch = $mrBranchFactory->getMrBranchOfCurrentHEAD();
+
+    $expectedBranchName = $mla_request->post['branch'];
+    // confirm we are on the correct branch
+    if ($currentMrBranch->getBranchName() != $expectedBranchName) {
+        throw new Exception(
+            message: "You are not on $expectedBranchName, but on " . $currentMrBranch->getBranchName()
+        );
+    }
     $deployer = new \QuickDeployer(debug: $mla_request->post['debug_deploy']);
-    $okay = $deployer->pushToBranch(newBranchName: $mla_request->post['branch']);
+    $okay = $deployer->pushToBranch(newBranchName: $expectedBranchName);
     if($okay)
     {
     }
