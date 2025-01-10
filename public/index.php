c252ba3f3d3c43e9eaf6cff881e63de6a5377d3d
Rob Nugen <rob@robnugen.com>
2025-01-10 22:23:46 +0900

    Send correct params to Template

--- public/index.php
+++ public/index.php
@@ -4,12 +4,12 @@
 include_once "/home/barefoot_rob/quick.robnugen.com/prepend.php";
 
 if($is_logged_in->isLoggedIn()){
-    $page = new \Template($mla_request, $mla_database, $config);
-    $page->set("text", "");  // index.tpl.php expects this
-    $page->setTemplate("poster/index.tpl.php");
+    $page = new \Template(config: $config);
+    $page->set(name: "text", value: "");  // index.tpl.php expects this
+    $page->setTemplate(template_file: "poster/index.tpl.php");
     $page->echoToScreen();
 } else {
-    $page = new \Template($mla_request, $mla_database, $config);
-    $page->setTemplate("login/index.tpl.php");
+    $page = new \Template(config: $config);
+    $page->setTemplate(template_file: "login/index.tpl.php");
     $page->echoToScreen();
 }
