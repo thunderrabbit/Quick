fb6783f2a5bc1f4ca97125cfaa9a6fc66efdcc32
Rob Nugen <rob@robnugen.com>
2025-01-22 10:48:17 +1000

    Allow deploy without posting
    Fixes #8

--- public/index.php
+++ public/index.php
@@ -4,7 +4,17 @@
 include_once "/home/barefoot_rob/quick.robnugen.com/prepend.php";
 
 if($is_logged_in->isLoggedIn()){
+    // Allow deploy without posting
+    $tempOSpooner = new TempOSpooner(
+        debugLevel: 0,
+    );
+
+    $gitLog = $tempOSpooner->getGitLog();
+    $show_deploy = true;
+    $mrBranchFactory = new MrBranchFactory(debugLevel: 0);
+    $newBranchName = $mrBranchFactory->getMrBranchOfCurrentHEAD();
     $page = new \Template(config: $config);
+    $page->set(name: "show_deploy", value: false);  // no need to deploy after we deploy
     $page->set(name: "text", value: "");  // index.tpl.php expects this
     $page->setTemplate(template_file: "poster/index.tpl.php");
     $page->echoToScreen();
