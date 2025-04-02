bd3a44d765ac795a5868ae6beabe0e17f7e6dfe9
Rob Nugen <rob@robnugen.com>
2025-04-02 08:50:11 +0800

    Add debugging to list/index.php

--- public/list/index.php
+++ public/list/index.php
@@ -4,13 +4,31 @@ include_once "/home/barefoot_rob/quick.robnugen.com/prepend.php";
 
 $page = new \Template(config: $config);
 
-$entries = [];
+$debug = isset($mla_request->get['debug']) ? (int) $mla_request->get['debug'] : 0;
 $year = $mla_request->get['year'] ?? date(format: 'Y');
 $month = $mla_request->get['month'] ?? '';
 
-if ($year !== '') {
-    $lister = new \QuickLister(journalRoot: $config->post_path_journal);
-    $entries = $lister->listEntries(year: $year, month: $month !== '' ? $month : null);
+if ($debug >= 1) {
+    echo "<div style='background:#eef;padding:10px;margin-bottom:1em;'>";
+    echo "<strong>Debug info:</strong><br>";
+    echo "Year: $year<br>";
+    echo "Month: $month<br>";
+}
+
+$lister = new \QuickLister(journalRoot: $config->post_path_journal);
+
+if ($debug >= 4) {
+    print_rob($lister, false);
+}
+
+$entries = $lister->listEntries(year: $year, month: $month !== '' ? $month : null);
+
+if ($debug >= 1) {
+    echo "Found " . count($entries) . " entries.<br>";
+    if ($debug >= 2) {
+        print_rob($entries, false);
+    }
+    echo "</div>";
 }
 
 $page->set(name: "year", value: $year);
