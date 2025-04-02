83751fa158354040ecfe9b6a98adb5dbaef804ff
Rob Nugen <rob@robnugen.com>
2025-04-02 08:44:25 +0800

    Prefer strings and default to current year

--- public/list/index.php
+++ public/list/index.php
@@ -5,16 +5,16 @@ include_once "/home/barefoot_rob/quick.robnugen.com/prepend.php";
 $page = new \Template(config: $config);
 
 $entries = [];
-$year = $mla_request->get['year'] ?? null;
-$month = $mla_request->get['month'] ?? null;
+$year = $mla_request->get['year'] ?? date(format: 'Y');
+$month = $mla_request->get['month'] ?? '';
 
-if ($year !== null) {
+if ($year !== '') {
     $lister = new \QuickLister(journalRoot: $config->post_path_journal);
-    $entries = $lister->listEntries(year: $year, month: $month);
+    $entries = $lister->listEntries(year: $year, month: $month !== '' ? $month : null);
 }
 
-$page->set(name: "entries", value: $entries);
 $page->set(name: "year", value: $year);
 $page->set(name: "month", value: $month);
+$page->set(name: "entries", value: $entries);
 $page->setTemplate(template_file: "list/index.tpl.php");
 $page->echoToScreen();
