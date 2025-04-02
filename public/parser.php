03d0706b2621025adf40e72f05ab314e52a9d6e3
Rob Nugen <rob@robnugen.com>
2025-04-02 14:04:29 +0800

    We can now edit files wowoww  but cannot save them yet boooo

--- public/parser.php
+++ public/parser.php
@@ -15,16 +15,9 @@ if (!$fullPath || !str_starts_with($fullPath, $config->post_path_journal)) {
 
 // require_once "{$config->app_path}/classes/QuickParser.php";
 
-// $parser = new \QuickParser($fullPath);
-// $data = $parser->parse();
+$parser = new \QuickParser($fullPath);
+$data = $parser->parse();
 
-$data = [
-    'title' => 'Sample Title',
-    'date' => 'Tuesday 1 February 2022',
-    'time' => '05:55',
-    'tags' => 'test, parser, debug',
-    'post_content' => "Sample content from the file.\n\nThis would be the body text.",
-];
 // Store parsed data in session and redirect to editor
 $_SESSION['edit_post_data'] = $data;
 
